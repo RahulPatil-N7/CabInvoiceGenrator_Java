be717e43fdc50a90b5713c0d974538333f82f9c0
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-08-07 00:05:24 +0530

    Added : calculate tota fare for Multiple rides.

---
+++ com/bridgelabz/CabInvoiceTool/Ride.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.CabInvoiceTool;
+
+public class Ride {
+	public final double distance;
+	public final int time;
+
+	/*
+	 * Ride constructor with parameters distance & time
+	 */
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
