dccc2b2ff189e964d69240f629d8804ae248eb02
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-08-06 19:46:38 +0530

    Added : printing welcome message.

---
+++ com/bridgelabz/CabInvoiceTool/CabInvoiceGeneratorMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.CabInvoiceTool;
+
+public class CabInvoiceGeneratorMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Cab Invoice Generator.\n");
+	}
+
+}
