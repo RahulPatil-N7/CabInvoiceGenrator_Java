cd5c271c18a217e35601638de5d00dfe3a052c55
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-08-06 23:05:34 +0530

    Added : Given distance & time, calculate total fare.

--- com/bridgelabz/CabInvoiceTool/CabInvoiceGeneratorMain.java
+++ com/bridgelabz/CabInvoiceTool/CabInvoiceGeneratorMain.java
@@ -1,9 +1,23 @@
 package com.bridgelabz.CabInvoiceTool;
 
 public class CabInvoiceGeneratorMain {
+	public final double COST_PER_KM = 10.0;
+	public final int COST_PER_MIN = 1;
+	private final double MIN_FARE = 5.0;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To Cab Invoice Generator.\n");
+	/*
+	 * This method calculates & returns total fare.
+	 */
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+		return Math.max(totalFare, MIN_FARE);
 	}
 
+	public static void main(String[] args) {
+		System.out.println("************** Welcome To Cab Invoice Generator. *************\n");
+
+		CabInvoiceGeneratorMain cabInvoice = new CabInvoiceGeneratorMain();
+		double result = cabInvoice.calculateFare(3.5, 30);
+		System.out.println("Total Fare :" + result + " Rs.");
+	}
 }
